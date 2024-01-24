4a6a8224c475a558db7611a173f708d956c887bd
Gabriel Brun <lydstyl@gmail.com>
2024-01-24 23:48:59 +0100

    Use prisma with Vercel Postgres db.

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,3 +1,7 @@
+// import prisma from '../lib/prisma'
+
+import prisma from '@/lib/prisma'
+
 export async function GET() {
   //   const res = await fetch('https://data.mongodb-api.com/...', {
   //     headers: {
@@ -7,6 +11,8 @@ export async function GET() {
   //   })
   //   const data = await res.json()
 
-  return Response.json({ hello: 'world' })
+  const fromPrisma = await prisma.skills.findMany()
+
+  return Response.json({ hello: fromPrisma })
   //   return Response.json({ data })
 }
