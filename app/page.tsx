05e488f98a92830f5f06350683c03144df8952eb
Gabriel Brun <lydstyl@gmail.com>
2024-01-25 01:37:11 +0100

    Fix build issue.

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <>
       <div className='flex'>
         <div className=''>
-          <h1 className='text-4xl font-bold'>Hello, I'm</h1>
+          <h1 className='text-4xl font-bold'>Hello, I&apos;m</h1>
           <h1 className='text-6xl font-bold'>Gabriel Brun</h1>
           <h1 className='text-4xl font-bold'>Full-Stack Developer</h1>
         </div>
@@ -13,7 +13,6 @@ export default function Home() {
           <Image
             src='/Gabriel_Brun.jpg'
             alt='Picture of the author'
-            // style={{ borderRadius: '50%' }}
             className='rounded-full w-60 h-60'
             width={400}
             height={400}
